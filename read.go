4e9283b817a02c26882593e6694f0a56d39a00c9
go-numb <gonumb66@gmail.com>
2024-02-21 17:06:14 +0900

    add check row has columns

--- read.go
+++ read.go
@@ -20,5 +20,14 @@ func (p *Client) Read(binder any) (dataframe.DataFrame, error) {
 		return df, SetError(err, "failed to bind data")
 	}
 
+	if len(records) <= 1 {
+		return df, SetError(fmt.Errorf("no data"), "failed to read data")
+	}
+	for i := 0; i < len(records); i++ {
+		if len(records[i]) != len(records[0]) {
+			return df, SetError(fmt.Errorf("invalid data, row has not required columns"), "failed to read data")
+		}
+	}
+
 	return dataframe.LoadRecords(records), nil
 }
